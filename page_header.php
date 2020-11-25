1781a96eafa4e72b59f6fb50a2e328fef3059903
janismasuhr <janismasuhr@aol.com>
2020-11-25 21:12:57 +0100

    Page Header und Footer

--- page_header.php
+++ page_header.php
@@ -3,12 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <title>Hermes Board</title>
-    <script src="js/bootstrap.js"></script>
-    <link rel="stylesheet" href="css/style.css" />
-    <link rel="stylesheet" href="css/bootstrap.css" />
+    <script src="/js/bootstrap.js"></script>
+    <link rel="stylesheet" href="/css/bootstrap.css" />
     <!-- Navigation Bar-->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <img src="img/logo/logo_small.png">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="index.php">Hermes-Board</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
