011a55db77255b10e5ee53e0fca12a29d9e3522f
janismasuhr <janismasuhr@aol.com>
2020-11-25 22:16:09 +0100

    NavBar und ähnliches

--- index.php
+++ index.php
@@ -20,7 +20,77 @@ include __DIR__ . '/page_header.php';
             <div class="card heel" >
                 <img class="card-img-top" src="img/reference/_MG_1188.jpg" alt="Card image">
                 <div class="card-img-overlay">
-                    <h4 class="card-title">h/lol</h4>
+                    <h4 class="card-title">h/earthporn</h4>
+                    <p class="card-text">earth, nature, pictures</p>
+                    <!-- <a href="#" class="btn btn-primary">See Profile</a> -->
+                </div>
+            </div>
+        </div>
+        <div class="col-sm">
+            <div class="card heel" >
+                <img class="card-img-top" src="img/reference/_MG_1330.jpg" alt="Card image">
+                <div class="card-img-overlay">
+                    <h4 class="card-title">h/flowers</h4>
+                    <p class="card-text">flowers, nature, beautiful</p>
+                    <!-- <a href="#" class="btn btn-primary">See Profile</a> -->
+                </div>
+            </div>
+        </div>
+        <div class="col-sm">
+            <div class="card heel" >
+                <img class="card-img-top" src="img/reference/_MG_1330.jpg" alt="Card image">
+                <div class="card-img-overlay">
+                    <h4 class="card-title">h/kappa</h4>
+                    <p class="card-text">Some example text.</p>
+                    <!-- <a href="#" class="btn btn-primary">See Profile</a> -->
+                </div>
+            </div>
+        </div>
+        <div class="col-sm">
+            <div class="card heel" >
+                <img class="card-img-top" src="img/reference/_MG_1330.jpg" alt="Card image">
+                <div class="card-img-overlay">
+                    <h4 class="card-title">h/kappa</h4>
+                    <p class="card-text">Some example text.</p>
+                    <!-- <a href="#" class="btn btn-primary">See Profile</a> -->
+                </div>
+            </div>
+        </div>
+        <div class="col-sm">
+            <div class="card heel" >
+                <img class="card-img-top" src="img/reference/_MG_1330.jpg" alt="Card image">
+                <div class="card-img-overlay">
+                    <h4 class="card-title">h/kappa</h4>
+                    <p class="card-text">Some example text.</p>
+                    <!-- <a href="#" class="btn btn-primary">See Profile</a> -->
+                </div>
+            </div>
+        </div>
+        <div class="col-sm">
+            <div class="card heel" >
+                <img class="card-img-top" src="img/reference/_MG_1330.jpg" alt="Card image">
+                <div class="card-img-overlay">
+                    <h4 class="card-title">h/kappa</h4>
+                    <p class="card-text">Some example text.</p>
+                    <!-- <a href="#" class="btn btn-primary">See Profile</a> -->
+                </div>
+            </div>
+        </div>
+        <div class="col-sm">
+            <div class="card heel" >
+                <img class="card-img-top" src="img/reference/_MG_1330.jpg" alt="Card image">
+                <div class="card-img-overlay">
+                    <h4 class="card-title">h/kappa</h4>
+                    <p class="card-text">Some example text.</p>
+                    <!-- <a href="#" class="btn btn-primary">See Profile</a> -->
+                </div>
+            </div>
+        </div>
+        <div class="col-sm">
+            <div class="card heel" >
+                <img class="card-img-top" src="img/reference/_MG_1330.jpg" alt="Card image">
+                <div class="card-img-overlay">
+                    <h4 class="card-title">h/kappa</h4>
                     <p class="card-text">Some example text.</p>
                     <!-- <a href="#" class="btn btn-primary">See Profile</a> -->
                 </div>
--- page_header.php
+++ page_header.php
@@ -6,25 +6,34 @@
     <script src="js/bootstrap.js"></script>
     <link rel="stylesheet" href="css/style.css" />
     <link rel="stylesheet" href="css/bootstrap.css" />
+    <link rel="shortcut icon" type="image/x-icon" href="img/logo/logo_tiny.png"
+
     <!-- Navigation Bar-->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <img src="img/logo/logo_small.png">
+        <!-- Logo und Schriftzug -->
+        <img class="navbar-brand" src="img/logo/logo_small.png">
         <a class="navbar-brand" href="index.php">Hermes-Board</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <!--<li class="nav-item">
-                    <a class="nav-link" href="#">Features</a>
-                </li>-->
-                <li class="nav-item">
-                    <a class="nav-link" href="mitglieder.php">xyz</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                </li>
-            </ul>
+        <!-- Searchbar -->
+        <div class="collapse navbar-collapse ml-5" id="navbarNav">
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="searchbar btn btn-outline-success my-2 my-sm-0 " type="submit">Search</button>
+            </form>
         </div>
+        <!-- DropDown -->
+        <li class="nav-item dropdown mr-5 ">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                login
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+        </li>
     </nav>
 </head>
