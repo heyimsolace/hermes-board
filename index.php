4b3eeba0222adbee0619331e72ffa718861cad39
heyimsolace <justin.derks01@gmail.com>
2020-11-25 20:48:02 +0100

    Anfänge sind langsam

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ include __DIR__ . '/page_header.php';
     <div class="row">
         <div class="col-sm">
             <div class="card heel" >
-                <img class="card-img-top" src="img/_MG_1155.jpg" alt="Card image">
+                <img class="card-img-top" src="img/reference/_MG_1155.jpg" alt="Card image">
                 <div class="card-img-overlay">
                     <h4 class="card-title">h/vinyls</h4>
                     <p class="card-text">vinyl, music, vintage</p>
@@ -18,7 +18,7 @@ include __DIR__ . '/page_header.php';
         </div>
         <div class="col-sm">
             <div class="card heel" >
-                <img class="card-img-top" src="img/_MG_1188.jpg" alt="Card image">
+                <img class="card-img-top" src="img/reference/_MG_1188.jpg" alt="Card image">
                 <div class="card-img-overlay">
                     <h4 class="card-title">h/lol</h4>
                     <p class="card-text">Some example text.</p>
@@ -28,7 +28,7 @@ include __DIR__ . '/page_header.php';
         </div>
         <div class="col-sm">
             <div class="card heel" >
-                <img class="card-img-top" src="img/_MG_1330.jpg" alt="Card image">
+                <img class="card-img-top" src="img/reference/_MG_1330.jpg" alt="Card image">
                 <div class="card-img-overlay">
                     <h4 class="card-title">h/kappa</h4>
                     <p class="card-text">Some example text.</p>
--- page_header.php
+++ page_header.php
@@ -7,7 +7,8 @@
     <link rel="stylesheet" href="css/style.css" />
     <link rel="stylesheet" href="css/bootstrap.css" />
     <!-- Navigation Bar-->
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <img src="img/logo/logo_small.png">
         <a class="navbar-brand" href="index.php">Hermes-Board</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
